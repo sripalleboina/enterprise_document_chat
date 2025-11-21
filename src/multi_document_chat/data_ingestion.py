cfc800ea25a440f88b0b292900a586c3444879e0
Sri <18306205+sripalleboina@users.noreply.github.com>
2025-11-21 10:42:30 -0500

    code structure updated for multi document

---
+++ src/multi_document_chat/data_ingestion.py
@@ -0,0 +1,10 @@
+class DocumentIngestor:
+    def __init__(self):
+        pass
+    
+    def ingest_files(self):
+        pass
+    
+    def _create_retriever(self, documents):
+        pass
+        
